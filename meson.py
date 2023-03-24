d2e104a0d14424d3c7bfb9aeadc85c3e79f11fdd
6yntar05 <6yntar05@mail.ru>
2023-03-24 20:58:43 +0700

    Minor changes

--- meson.py
+++ meson.py
@@ -3,6 +3,8 @@ import os, sys, shutil
 
 try:
     for i in ("shaders", "textures"):
+        shutil.rmtree(
+            os.path.join(os.getenv('MESON_BUILD_ROOT'), i))
         shutil.copytree(
             os.path.join(os.getenv('MESON_SOURCE_ROOT'), "res", i),
             os.path.join(os.getenv('MESON_BUILD_ROOT'), i))
