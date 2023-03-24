2c58b92a67366899c0c5ad30f7f890ff8e1ae9b0
6yntar05 <6yntar05@mail.ru>
2023-03-24 21:00:49 +0700

    Minor changes

--- meson.py
+++ meson.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 import os, sys, shutil
 
-try:
-    for i in ("shaders", "textures"):
-        shutil.rmtree(
-            os.path.join(os.getenv('MESON_BUILD_ROOT'), i))
-        shutil.copytree(
-            os.path.join(os.getenv('MESON_SOURCE_ROOT'), "res", i),
-            os.path.join(os.getenv('MESON_BUILD_ROOT'), i))
-except FileExistsError:
-    exit(0)
+for i in ("shaders", "textures"):
+    shutil.copytree(
+        os.path.join(os.getenv('MESON_SOURCE_ROOT'), "res", i),
+        os.path.join(os.getenv('MESON_BUILD_ROOT'), i),
+        dirs_exist_ok=True)
