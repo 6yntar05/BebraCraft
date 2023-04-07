d6c4d920efacff5c8c1d93332aa32060185b5d48
6yntar05 <6yntar05@mail.ru>
2023-04-07 21:02:51 +0700

    dev: Improved internal VBO and meshes view

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,4 +1,3 @@
-#include "SDL_video.h"
 #include "engine/camera.h"
 #include "engine/core.h"
 #include "engine/graphics/shaders.h"
@@ -115,12 +114,9 @@ int main(int argc, char* argv[]) {
 		screen.gbuffer->bind();
         screen.clear();
         skybox.render(viewIdenpedent, projection, rawTime);
-
         if (window.debug.lines)
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);    
 
-        // TODO: chunks loader -> mesh
-        //for (int x = 0; x < 16; x++) for (int y = 0; y < 16; y++) // CALLS OVERHEAD
         { // Chunks render
             blockShader.use();
             blockShaderSet.model(model);
@@ -129,27 +125,21 @@ int main(int argc, char* argv[]) {
             blockShaderSet.worldTime(rawTime);
             static auto cameraBlocksPos = glm::value_ptr(camera.pos);
 
-            // Functor: Render single layer of chunk
-            static std::function layerFunctor = [&](bebra::objects::chunk& chunk, int iLayer) {
+            for (int iLayer = 0; iLayer < chunkSize ; iLayer++) {
                 bebra::objects::chunkLayer& layer = chunk.at(iLayer);
-
-                // Functor: Render single row of chunk 
-                static std::function rowFunctor = [&](bebra::objects::chunkLayer& layer, int iRow) {
+                for (int iRow = 0; iRow < 16; iRow++) {
                     bebra::objects::chunkRow& row = layer.at(iRow);
-                    
-                    // Functor: Render single block of chunk
-                    static std::function blockFunctor = [&](bebra::objects::chunkRow& row, int iBlock) {
+                     for (int iBlock = 0; iBlock < 16; iBlock++) {
                         const bebra::objects::Object* block = row.at(iBlock);
                         
                         // Check for visible
-                        if (!block->texture.arraySize) return;
+                        if (!block->texture.arraySize) continue;
 
                         // Block space transformation
                         glm::mat4 model = glm::mat4(1.0f);
-                        //model = glm::translate(model, { iBlock + 16*x, iLayer, iRow + 16*y });
                         model = glm::translate(model, { iBlock, iLayer, iRow });
-                        if (block->rotate != 0.0)
-                            model = glm::rotate(model, glm::radians(block->rotate), { 0.0, 1.0, 0.0 });
+                        //if (block->rotate != 0.0)
+                        //    model = glm::rotate(model, glm::radians(block->rotate), { 0.0, 1.0, 0.0 });
                         blockShaderSet.model(model);
 
                         // Pick right buffers for current object
@@ -171,64 +161,46 @@ int main(int argc, char* argv[]) {
                             glDrawArrays(GL_TRIANGLES, 0, 12);
                             glEnable(GL_CULL_FACE);
                         } else {
-                            //glDrawArrays(GL_TRIANGLES, 0, 36);
+                            glDrawArrays(GL_TRIANGLES, 0, 36);
                         }
                         chunkCallsCounter++;
 
                         // Mesh test:
                         testmesh.render();
 
-                    };
-                    // TODO: inverse for depth-test optimization
-                    //-Y[->>Camera   ]+Y
-                    for (int iBlock = 0; iBlock < std::min(std::max(0, static_cast<int>(std::round(cameraBlocksPos[0]))), 16); iBlock++)
-                        blockFunctor(row, iBlock);
-                    //-Y[   Camera<<-]+Y
-                    for (int iBlock = 15; iBlock >= std::max(0, static_cast<int>(std::round(cameraBlocksPos[0]))); iBlock--)
-                        blockFunctor(row, iBlock);
-                };
-
-                //-X[->>Camera   ]+X
-                for (int iRow = 0; iRow < std::min(std::max(0, static_cast<int>(std::round(cameraBlocksPos[2]))), 16); iRow++)
-                    rowFunctor(layer, iRow);
-                //-X[   Camera<<-]+X
-                for (int iRow = 15; iRow >= std::max(0, static_cast<int>(std::round(cameraBlocksPos[2]))); iRow--)
-                    rowFunctor(layer, iRow);
-            };
-            //DN[->>Camera   ]UP
-            for (int iLayer = 0; iLayer < std::min(std::max(0, static_cast<int>(std::round(cameraBlocksPos[1]))), chunkSize) ; iLayer++)
-                layerFunctor(chunk, iLayer);
-            //DN[   Camera<<-]UP
-            for (int iLayer = chunkSize-1; iLayer >= std::min(std::max(0, static_cast<int>(std::round(cameraBlocksPos[1]))), chunkSize); iLayer--)
-                layerFunctor(chunk, iLayer);
+                    }
+                }
+            }
         }
 
-        // Render from G-Buffer
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        screen.gbuffer->unbind();
-        screen.render(!window.debug.nohud);
+        {// Render from G-Buffer
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            screen.gbuffer->unbind();
+            screen.render(!window.debug.nohud);
+        }
 
-        // Calculate frametime & FPS
-        auto end = SDL_GetPerformanceCounter();
-        float Frametime = ((end - start) * 1000.0) / (float)SDL_GetPerformanceFrequency();
-        if (Frametime > maxFrametime)
-            maxFrametime = Frametime;
-        else
-            maxFrametime = ((maxFrametime * 50.0) + Frametime) / 51.0;
-        start = end;
+        { // Calculate frametime & FPS
+            auto end = SDL_GetPerformanceCounter();
+            float Frametime = ((end - start) * 1000.0) / (float)SDL_GetPerformanceFrequency();
+            if (Frametime > maxFrametime)
+                maxFrametime = Frametime;
+            else
+                maxFrametime = ((maxFrametime * 50.0) + Frametime) / 51.0;
+            start = end;
 
-        // Render HUD
-        if (!window.debug.nohud) {
-            static std::function topOffset = [&](uint lineFromTop) {
-                return float(window.mode.h) - ((float(std::max(text.width, text.height)) + 5.0) * (lineFromTop + 1));
-            };
+            // Render HUD
+            if (!window.debug.nohud) {
+                static std::function topOffset = [&](uint lineFromTop) {
+                    return float(window.mode.h) - ((float(std::max(text.width, text.height)) + 5.0) * (lineFromTop + 1));
+                };
 
-            text.render("Frametime: " + std::to_string(Frametime) + "ms" +
-                        " / 2%Max: " + std::to_string(maxFrametime) + "ms",
-                        projectionFont, 10.0, topOffset(0));
-            text.render("Graphics calls: " + std::to_string(chunkCallsCounter), projectionFont, 10.0, topOffset(1));
-            text.render("BebraCraft pre-alpha: " + std::string(__DATE__), projectionFont, 10.0, topOffset(2));
-            text.render("Testchunk", projectionFont, 10.0, topOffset(3));
+                text.render("Frametime: " + std::to_string(Frametime) + "ms" +
+                            " / 2%Max: " + std::to_string(maxFrametime) + "ms",
+                            projectionFont, 10.0, topOffset(0));
+                text.render("Graphics calls: " + std::to_string(chunkCallsCounter), projectionFont, 10.0, topOffset(1));
+                text.render("BebraCraft pre-alpha: " + std::string(__DATE__), projectionFont, 10.0, topOffset(2));
+                text.render("Testchunk", projectionFont, 10.0, topOffset(3));
+            }
         }
 
         bebra::utils::glHandleError(glGetError());
